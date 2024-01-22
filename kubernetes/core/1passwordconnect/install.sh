19bcd3fa218a4d51d3bca7381be564ae6bd58fd4
krangence <matt@matt-white.co.uk>
2024-01-22 00:20:40 +0000

    configuration: updating configuration for installation of 'core' services

--- kubernetes/core/1passwordconnect/install.sh
+++ kubernetes/core/1passwordconnect/install.sh
@@ -9,11 +9,11 @@ else
 fi
 
 # Set our variables
-HELM_REPO_NAME="cert-manager"
-HELM_REPO_SOURCE="https://charts.jetstack.io"
-HELM_APP_NAME="cert-manager"
-HELM_APP_VERSION="1.13.3"
-HELM_APP_NAMESPACE="kube-system"
+HELM_REPO_NAME="1password"
+HELM_REPO_SOURCE="https://1password.github.io/connect-helm-charts"
+HELM_APP_NAME="connect"
+HELM_APP_VERSION="1.14.0"
+HELM_APP_NAMESPACE="1passwordconnect"
 
 # Add the Helm repository
 helm repo add ${HELM_REPO_NAME} ${HELM_REPO_SOURCE}
@@ -21,7 +21,7 @@ helm repo add ${HELM_REPO_NAME} ${HELM_REPO_SOURCE}
 helm repo update
 # Template out and install the Helm chart via pipe to `kubectl apply`
 helm template ${HELM_APP_NAME} ${HELM_REPO_NAME}/${HELM_APP_NAME} \
---version ${HELM_APP_NAME} \
+--version ${HELM_APP_VERSION} \
 --values values.yaml \
 -n ${HELM_APP_NAMESPACE} | \
 kubectl apply -f - --dry-run=${DRY_RUN}
