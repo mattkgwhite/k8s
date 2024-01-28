744e7f6b3d8bd54f64bc4fbaf2a796c5aa28c1cf
KayGee <matt@matt-white.co.uk>
2024-01-28 00:27:19 +0000

    external-dns: update deployment configuration

--- kubernetes/core/external-dns/install.sh
+++ kubernetes/core/external-dns/install.sh
@@ -12,8 +12,8 @@ fi
 HELM_REPO_NAME="external-dns"
 HELM_REPO_SOURCE="https://kubernetes-sigs.github.io/external-dns/"
 HELM_APP_NAME="external-dns"
-HELM_APP_VERSION="1.13.3"
-HELM_APP_NAMESPACE="kube-system"
+HELM_APP_VERSION="1.14.3"
+HELM_APP_NAMESPACE="external-dns"
 
 # Add the Helm repository
 helm repo add ${HELM_REPO_NAME} ${HELM_REPO_SOURCE}
