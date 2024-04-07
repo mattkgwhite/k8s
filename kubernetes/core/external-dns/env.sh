10aceb3b852636bbcad730c5759589847e32870b
KayGee <matt@matt-white.co.uk>
2024-04-07 14:40:16 +0100

    external-dns: update apps and core references

--- kubernetes/core/external-dns/env.sh
+++ kubernetes/core/external-dns/env.sh
@@ -1,4 +1,4 @@
-HELM_APP_NAMESPACE="kube-system"
+HELM_APP_NAMESPACE="external-dns"
 HELM_REPO_NAME="external-dns"
 HELM_REPO_SOURCE="https://kubernetes-sigs.github.io/external-dns/"
 HELM_APP_NAME="external-dns"
